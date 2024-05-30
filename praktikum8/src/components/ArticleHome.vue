da1e8f3b01d2a2472a17d779208a03728c7ff358
riyandaaziz123 <riyandaazizi03@gmail.com>
2024-05-30 17:08:42 +0700

    vue

---
+++ praktikum8/src/components/ArticleHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img src="@/assets/Justin-Lahksana.jpg" alt="" width="400" height="200">
+        <h1>Ini adalah komponen article</h1>
+        <p>ini adalah isi article </p>
+        <p>ini adalah isi article </p>
+        <p>ini adalah isi article </p>
+        <p>ini adalah isi article </p>
+    </div>
+</template>
---
+++ praktikum8/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArticleHome from '../components/ArticleHome.vue'
+
+</script>
+
+<template>
+  <main>
+   <!-- --<TheWelcome /> -- -->
+   <h1>CUSROM XIOMI</h1>
+   <ArticleHome/>
+  </main>
+</template>
